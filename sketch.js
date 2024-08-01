263b8195518a6b1b3afc618be29c05de671356aa
Isa1253 <00001119304994sp@al.educacao.sp.gov.br>
2024-08-01 18:42:28 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(800, 500);
+  background("#1EAFA2");
+}
+
+
+function draw() {
+
+  
+fill ("#00BCD4");
+stroke ("lightblue")
+if (mouseIsPressed)
+rect(mouseX,mouseY,30,50);
+  
+}
